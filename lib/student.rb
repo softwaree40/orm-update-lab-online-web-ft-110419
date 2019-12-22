36a0c5c84bf58760de287fd806d10084d236c8e1
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 06:27:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,5 +58,21 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-   
+    def self.create(name, grade)
+    new_student = self.new(name, grade)
+    new_student.save
+    new_student
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+   self.new(name, grade, id)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ? ORDER BY students.id LIMIT 1"
+    DB[:conn].execute(sql, name).collect { |row| self.new_from_db(row) }.first
+  end
 end
