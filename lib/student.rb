c3dc12c25f0511993794a0bbfe088947df907134
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 06:26:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,4 +53,9 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+   def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
