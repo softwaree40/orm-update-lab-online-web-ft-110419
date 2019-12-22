76aab4597811fda72f84792fe87c1074425711bb
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 05:57:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
    end
    def self.create_table
             sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS student (
+      CREATE TABLE  students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
