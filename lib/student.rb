2c3043a21be5bb9344f0a38a4db21a89a9b42a23
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 05:55:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,8 @@
 require_relative "../config/environment.rb"
 require "pry"
 class Student
-   attr_accessor :name,:grade,:id
+   attr_accessor :name,:grade
+   attr_reader:id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
    def initialize(name,grade,id =nil)
