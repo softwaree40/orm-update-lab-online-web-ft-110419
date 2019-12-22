a6b0f8c1cb767b5be7fd52f2430f6bfa5a397ef9
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 05:49:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,24 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+   attr_accessor :name,:grade,:id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+   def initialize(name,grade,id =nil)
+       @name = name 
+       @grade = grade 
+       @id = id
+   end
+   def self.create_table
+            sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS student (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql).map do ||
+     
+    end
 
 end
