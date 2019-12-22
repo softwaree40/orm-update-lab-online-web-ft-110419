9c40a6b812fa198e3d022a2a60623a9f2968f14a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 06:08:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,8 +24,11 @@ class Student
        
     end
     def self.drop_table
-        sql = <<- SQL DROP TABLE students SQL
-       DB[:conn].execute(sql)
+        sql = <<-SQL 
+        DROP TABLE students
+        
+        SQL
+        DB[:conn].execute(sql)
     end
 
 end
